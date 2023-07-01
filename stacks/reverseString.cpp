4e5cbb1fe1a058e23d54050fb59c33c3853f931c
cypher1002 <mehuldarshni42@gmail.com>
2023-07-01 17:29:10 +0530

    stack using linked-list

---
+++ stacks/reverseString.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stack>
+#include<iostream>
+using namespace std;
+
+int main(){
+   
+   string s = "Mehul";
+   stack<char>s;
+
+   for(int i =0;i< s.length(); i++){
+       char st = s[i];
+       s.push()
+   }
+
+
+    return 0;
+}
